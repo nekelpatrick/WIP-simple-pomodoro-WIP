5277cbf241e5aaeb979b72f064d465d69e8922ea
Patrick Nekel <nekelpatrick@gmail.com>
2022-08-03 22:29:12 -0300

    :lipstick: finish homepage UI

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,12 +15,25 @@ export const Home = () => {
       <form action="">
         <FormContainer>
           <label htmlFor="">I will focus on</label>
-          <TaskInput id="task" placeholder="Name your task" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Name your task"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+          </datalist>
+
           <label htmlFor="">for</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutes.</span>
         </FormContainer>
