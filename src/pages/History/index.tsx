75380b710e3e854fa1e31dd8eb79ecb8e75203d3
Patrick Nekel <nekelpatrick@gmail.com>
2022-08-04 21:58:30 -0300

    :lipstick: add the status colored circle

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export const History = () => {
   return (
@@ -21,28 +21,36 @@ export const History = () => {
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="green">Done</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="green">Done</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="yellow">Done</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="red">Done</Status>
+              </td>
             </tr>
           </tbody>
         </table>
